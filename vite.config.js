3218b8b23992724d2edf2d8259c3e4f4a066b656
Mobasshir <mobasshirhussain2002@gmail.com>
2025-08-18 14:44:06 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/notflix-clone-ui/",
+  base: "/movie-site/",
   plugins: [react()],
 })
